c0f22e3045a2b296a5f8d9b89a5c01a4adbb6a92
anurag mishra <anurag171@gmail.com>
2022-05-12 18:12:55 +0530

    Api changes

--- elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
@@ -1,30 +1,47 @@
 package com.microservices.demo.elastic.query.service.config;
 
+import com.microservices.demo.config.UserConfigData;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+  private final UserConfigData userConfigData;
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
         .httpBasic()
         .and()
         .authorizeRequests()
-        .antMatchers("/**").hasRole("USERS")
+        .antMatchers("/**").hasRole("USER")
         .and()
         .csrf().disable();
   }
 
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    log.info("Print password [{}]",userConfigData.getPassword());
     auth
         .inMemoryAuthentication()
-            .withUser("test")
-                .password("qwe123")
-                    .roles("USER");
+            .withUser(userConfigData.getUsername())
+                .password(passwordEncoder().encode(userConfigData.getPassword()))
+                    .roles(userConfigData.getRoles());
+  }
+
+  @Bean
+  protected PasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
   }
 }
